83d749e7702de8118dca634e11261704d1ffa2d4
Jonathan Forby <jonathan@forby.co>
2020-06-09 18:42:42 -0400

    working draft

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,37 @@
+import { gsap, Power2 } from 'gsap'
+import { ScrollToPlugin } from 'gsap/all'
+gsap.registerPlugin(ScrollToPlugin)
+
+var windowScrollTime = 1.2;
+var windowScrollEasing = Power2.easeInOut;
+
 function setupMenuToggle() {
     var toggle = document.getElementById('menu-toggle');
     var target = document.getElementById('menu-target');
-    toggle.addEventListener("click", function(el) {
+    toggle.addEventListener("click", function(event) {
         target.classList.toggle('open');
+    });
+}
+
+function setupNavigation() {
+    document.querySelectorAll('.nav-anchor').forEach((element, index) => {
+        element.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            var header = document.querySelector('header').offsetHeight;
+            var href = event.target.getAttribute('href');
+
+            if (document.querySelector(href) != null) {
+                gsap.to(window, {duration:windowScrollTime, scrollTo: {y: href, offsetY: header}, ease: windowScrollEasing});
+            } else {
+                console.log('nope');
+            }
+        });
     });
 }
 
 // GO
 window.addEventListener('load', function() {
     setupMenuToggle();
+    setupNavigation();
 });
